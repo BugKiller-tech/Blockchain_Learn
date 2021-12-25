95da4b7a12fe565b01c8f6ec4c660754943d618f
LeadDev <25556402+BugKiller-tech@users.noreply.github.com>
2021-12-25 08:22:26 +0000

    done educational simple coin implementation

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 const crypto = require('crypto-js')
 
 /**
- * computer is so fast these days so calculating hash is not a problem.
- * we need to prevent and it's called something like proof of work
- * ex; bitcoin requires the hash of the block begin with certain amount of 0, because we can't implement the output of the hash
+ * make cryto
+ * 1. make block can have multiple transactions
+ * 2. add rewards for miners
  * 
  */
+
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
+
 class Block {
-  constructor(index, timestamp, data, previousHash = '') {
-    this.index = index;
+  constructor(timestamp, transactions, previousHash = '') {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transactions = transactions;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
 
-    /**
-     * why ? above index, timestamp, data, prevoiusHash is transaction so we can't change it. so we need the thing we can change.
-     * generally 
-     */
     this.nonce = 0; 
   }
   calculateHash() {
@@ -39,20 +43,51 @@ class Blockchain {
   constructor() {
     this.chain = [ this.createGenesisBlock() ];
     this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
   }
 
   createGenesisBlock () {
-    return new Block(0, "12/21/2021", "Genesis block", "0")
+    return new Block("12/21/2021", "Genesis block", "0")
   }
 
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.mineBlock(this.difficulty);
-    this.chain.push(newBlock); // in reality, we can't add block easily. it will need to have some validation
+  minePendingTransactions(miningRewardAddress) {
+    /**
+     * bitcoin, too many transactions, will fetch few.
+     */
+    let block = new Block(Date.now(), this.pendingTransactions);
+    block.mineBlock(this.difficulty);
+    console.log('Block successfully mined!');
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ]
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
+        }
+
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+    return balance;
   }
 
 
@@ -72,9 +107,14 @@ class Blockchain {
 }
 
 
-let hChain = new Blockchain();
-console.log('Mining block 1...');
-hChain.addBlock(new Block(1, "12/21/2021", { amount: 4 }));
-console.log('Mining block 2...');
-hChain.addBlock(new Block(2, "12/21/2021", { amount: 10 }));
+let hCoin = new Blockchain();
+hCoin.createTransaction(new Transaction('address1', 'address2', 100));
+hCoin.createTransaction(new Transaction('address2', 'address1', 50));
 
+console.log('Starting miner');
+hCoin.minePendingTransactions('hkg-address');
+console.log('Balance of hkg', hCoin.getBalanceOfAddress('hkg-address'));
+
+console.log('Starting miner');
+hCoin.minePendingTransactions('hkg-address');
+console.log('Balance of hkg', hCoin.getBalanceOfAddress('hkg-address'));
