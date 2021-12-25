35a571a43387d5fe0b13e5bbdf9b1c008f396c5d
LeadDev <25556402+BugKiller-tech@users.noreply.github.com>
2021-12-25 07:16:09 +0000

    done

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-// NFT Non-Fungible Token
-
 const crypto = require('crypto-js')
+
+/**
+ * computer is so fast these days so calculating hash is not a problem.
+ * we need to prevent and it's called something like proof of work
+ * ex; bitcoin requires the hash of the block begin with certain amount of 0, because we can't implement the output of the hash
+ * 
+ */
 class Block {
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
@@ -8,15 +13,32 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+
+    /**
+     * why ? above index, timestamp, data, prevoiusHash is transaction so we can't change it. so we need the thing we can change.
+     * generally 
+     */
+    this.nonce = 0; 
   }
   calculateHash() {
-    return crypto.SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    return crypto.SHA256(this.index + this.previousHash + this.timestamp
+                        + JSON.stringify(this.data) + this.nonce).toString()
+  }
+
+  mineBlock (difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [ this.createGenesisBlock() ];
+    this.difficulty = 2;
   }
 
   createGenesisBlock () {
@@ -29,8 +51,7 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    console.log('prev hash is', newBlock.previousHash)
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock); // in reality, we can't add block easily. it will need to have some validation
   }
 
@@ -52,29 +73,8 @@ class Blockchain {
 
 
 let hChain = new Blockchain();
+console.log('Mining block 1...');
 hChain.addBlock(new Block(1, "12/21/2021", { amount: 4 }));
+console.log('Mining block 2...');
 hChain.addBlock(new Block(2, "12/21/2021", { amount: 10 }));
 
-console.log(JSON.stringify(hChain, null, 4));
-
-console.log('Is block chain valid ? ' + hChain.isChainValid());
-
-hChain.chain[1].data = { amount: 100 }
-// hChain.chain[1].hash = hChain.chain[1].calculateHash(); // if we recalculate then ok ? NOPE
-console.log('Is block chain valid ?? ' + hChain.isChainValid());
-
-// if something broken block chain, then we need a way to rollback of the chain.
-
-
-// let proofOfWork = 0
-// while(true) {
-//   proofOfWork++;
-//   let hash = crypto.SHA256("some data here"+proofOfWork).toString();
-//   console.log(proofOfWork, hash)
-//   // if (proofOfWork > 100) {
-//   //   break;
-//   // }
-//   if (hash.startsWith("000")) {
-//     break;
-//   }
-// }
